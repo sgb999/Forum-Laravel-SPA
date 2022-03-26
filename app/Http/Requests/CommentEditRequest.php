95257a97eafe3c250703de8c1bb25b2540e4c27a
sgb999 <59648138+sgb999@users.noreply.github.com>
2022-03-26 22:21:06 +0000

    installed PSR-12 composer package

--- app/Http/Requests/CommentEditRequest.php
+++ app/Http/Requests/CommentEditRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
@@ -8,23 +10,19 @@ class CommentEditRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
-     * @return bool
      */
-    public function authorize(): bool
+    public function authorize() : bool
     {
         return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
-     * @return array
      */
-    public function rules(): array
+    public function rules() : array
     {
         return [
-            'comment' => ['required', 'string']
+            'comment' => ['required', 'string'],
         ];
     }
 }
--- app/Models/TemporaryFile.php
+++ app/Models/TemporaryFile.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,6 +13,6 @@ class TempoarayFile extends Model
 
     protected $fillable = [
         'filename',
-        'folder'
+        'folder',
     ];
 }
